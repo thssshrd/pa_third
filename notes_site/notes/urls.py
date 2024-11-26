0ea777ec39d08cb9b4577345ca59448fa1139cf0
Mike <mikhail.strelnikov.ms@gmail.com>
2024-11-26 19:12:05 +0300

    Обновил url для редактирования задачи

--- notes_site/notes/urls.py
+++ notes_site/notes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('add/', views.add_note, name='add_note'),
-    path('edit/<uuid:note_id>/', views.edit_note, name='edit_note'),
+    path('<uuid:note_id>/', views.edit_note, name='edit_note'),
     path('delete/<uuid:note_id>/', views.delete_note, name='delete_note'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
